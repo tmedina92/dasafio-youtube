69936e97b2ecd6b4430334a47008ebe53fa02ef2
thamis medina <thamismedina@gmail.com>
2022-04-18 22:14:19 -0300

    commitado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,62 @@
+import logoImg from '../assets/imagem/logo.svg';
+
 export function Home() {
   return (
     <div>
       <header>
         <div>
-          <a href="/">Youtube</a>
+          <a href="/"><img src={logoImg} alt="Youtube"/></a>
           <p>Broadcast Yourself</p>
         </div>
         <div>
           <input 
             type="text" 
           />
-          <button
-            name="Search"
-          />
+          <button>Search</button>
           <a href="">Home</a>
           <a href="">Vídeos</a>
           <a href="">Channels</a>
         </div>
+        <div>
+          <a href="">Create Account</a>
+          <p>or</p>
+          <a href="">Sign-In</a>
+          <a href="">Subscriptions</a>
+          <a href="">History</a>
+          <a href="">Upload</a>
+        </div>
       </header>
-    
+      <aside>
+        <div>
+          <h2>Featured</h2>
+          <video src=""></video>
+        </div>
+        <div>
+          <video src=""></video>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente incidunt fugiat nisi officia dolore rerum illum dignissimos cum quibusdam, in quasi, iusto vero animi ut natus maiores dolores, odio rem.</p>
+          <video src=""></video>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente incidunt fugiat nisi officia dolore rerum illum dignissimos cum quibusdam, in quasi, iusto vero animi ut natus maiores dolores, odio rem.</p>
+          <video src=""></video>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente incidunt fugiat nisi officia dolore rerum illum dignissimos cum quibusdam, in quasi, iusto vero animi ut natus maiores dolores, odio rem.</p>
+        </div>
+      </aside>
+      <aside>
+        <img src="https://images.unsplash.com/photo-1519215498729-0abf8367b98c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YW51bmNpb3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae tempore error eius a temporibus sed, veritatis soluta ipsam dolores facere! Fugiat neque quod ex minima, eum minus impedit quos repellendus?</p>
+      </aside>
+      <main>
+        <h2>More Videos</h2>
+        <video src=""></video>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum incidunt quae quod quos totam reprehenderit, mollitia aliquid vero rerum dolore enim, ratione esse iste nemo iure, nulla provident exercitationem.</p>
+        <video src=""></video>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum incidunt quae quod quos totam reprehenderit, mollitia aliquid vero rerum dolore enim, ratione esse iste nemo iure, nulla provident exercitationem.</p><video src=""></video>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum incidunt quae quod quos totam reprehenderit, mollitia aliquid vero rerum dolore enim, ratione esse iste nemo iure, nulla provident exercitationem.</p><video src=""></video>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum incidunt quae quod quos totam reprehenderit, mollitia aliquid vero rerum dolore enim, ratione esse iste nemo iure, nulla provident exercitationem.</p><video src=""></video>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum incidunt quae quod quos totam reprehenderit, mollitia aliquid vero rerum dolore enim, ratione esse iste nemo iure, nulla provident exercitationem.</p><video src=""></video>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum incidunt quae quod quos totam reprehenderit, mollitia aliquid vero rerum dolore enim, ratione esse iste nemo iure, nulla provident exercitationem.</p><video src=""></video>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum incidunt quae quod quos totam reprehenderit, mollitia aliquid vero rerum dolore enim, ratione esse iste nemo iure, nulla provident exercitationem.</p><video src=""></video>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi illum incidunt quae quod quos totam reprehenderit, mollitia aliquid vero rerum dolore enim, ratione esse iste nemo iure, nulla provident exercitationem.</p>
+      </main>
     </div>
   )
 }
