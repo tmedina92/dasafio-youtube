a0ee87304cdcc302fefb46a5ace38cf7a2e60bc7
thamis medina <thamismedina@gmail.com>
2022-04-18 21:12:54 -0300

    commitado

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+export function Home() {
+  return (
+    <div>
+      <header>
+        <div>
+          <a href="/">Youtube</a>
+          <p>Broadcast Yourself</p>
+        </div>
+        <div>
+          <input 
+            type="text" 
+          />
+          <button
+            name="Search"
+          />
+          <a href="">Home</a>
+          <a href="">Vídeos</a>
+          <a href="">Channels</a>
+        </div>
+      </header>
+    
+    </div>
+  )
+}
