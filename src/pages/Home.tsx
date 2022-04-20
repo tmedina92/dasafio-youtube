d350664976172582e085c5b3f33d00c3698b6af6
thamis medina <thamismedina@gmail.com>
2022-04-20 01:01:58 -0300

    commitado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,10 +36,55 @@ export function Home() {
           </div>
         </div>
       </header>
-      <main>
-        <img src="https://images.unsplash.com/photo-1650287996854-bdfeaa5fce94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-        <img src="https://images.unsplash.com/photo-1650287996854-bdfeaa5fce94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" /><img src="https://images.unsplash.com/photo-1650287996854-bdfeaa5fce94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" /><img src="https://images.unsplash.com/photo-1650287996854-bdfeaa5fce94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" /><img src="https://images.unsplash.com/photo-1650287996854-bdfeaa5fce94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" /><img src="https://images.unsplash.com/photo-1650287996854-bdfeaa5fce94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" /><img src="https://images.unsplash.com/photo-1650287996854-bdfeaa5fce94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" /><img src="https://images.unsplash.com/photo-1650287996854-bdfeaa5fce94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" /><img src="https://images.unsplash.com/photo-1650287996854-bdfeaa5fce94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+      <main className="featured">
+        <div className="featured-wrapper">
+          <div className="featured-first">
+            <h2>Featured</h2>
+            <img src="https://images.unsplash.com/photo-1650305633654-3fec0ea2fb8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="exemplo" />
+          </div>
+          <div className="featured-second">
+            <div className="first-video">
+              <img src="https://images.unsplash.com/photo-1650296399459-975aac9856c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ab rem sequi at saepe non? </p>
+            </div>
+            <div className="first-video">
+              <img src="https://images.unsplash.com/photo-1650296399459-975aac9856c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ab rem sequi at saepe non? </p>
+            </div>
+            <div className="first-video">
+              <img src="https://images.unsplash.com/photo-1650296399459-975aac9856c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ab rem sequi at saepe non? </p>
+            </div>
+          </div>
+        </div>
+        <div className="square-wrapper">
+          <img src="https://images.unsplash.com/photo-1650296399459-975aac9856c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum itaque eius quas harum earum commodi, corporis fuga animi quae ab obcaecati hic, eos repellat iusto. Tempore commodi facere culpa maxime.</p>
+        </div>
       </main>
+      <div className="more-videos">
+        <h2>More videos</h2>
+        <div className="first-videos">
+          <img src="https://images.unsplash.com/photo-1650296399459-975aac9856c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ab rem sequi at saepe non? </p>
+          <img src="https://images.unsplash.com/photo-1650296399459-975aac9856c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ab rem sequi at saepe non? </p>
+          <img src="https://images.unsplash.com/photo-1650296399459-975aac9856c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ab rem sequi at saepe non? </p>
+          <img src="https://images.unsplash.com/photo-1650296399459-975aac9856c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ab rem sequi at saepe non? </p>
+        </div>
+        <div className="second-videos">
+          <img src="https://images.unsplash.com/photo-1650296399459-975aac9856c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ab rem sequi at saepe non? </p>
+          <img src="https://images.unsplash.com/photo-1650296399459-975aac9856c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ab rem sequi at saepe non? </p>
+          <img src="https://images.unsplash.com/photo-1650296399459-975aac9856c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ab rem sequi at saepe non? </p>
+          <img src="https://images.unsplash.com/photo-1650296399459-975aac9856c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ab rem sequi at saepe non? </p>
+        </div>
+      </div>
     </div>
   )
 }
